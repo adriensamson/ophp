a616200b2107e6fb56d34b804860aea96517f7f8
Adrien Samson <adrien@kyklydse.com>
2013-04-29 18:46:23 +0200

    Pack extensions

--- src/main.ml
+++ src/main.ml
@@ -1,13 +1,7 @@
 let main () =
     let self = FileUtil.readlink Sys.executable_name in
     let baseDir = String.sub self 0 (String.rindex self '/') in
-    Interpreter.Main.addExtension (baseDir ^ "/extensions/core/misc.cmo");
-    Interpreter.Main.addExtension (baseDir ^ "/extensions/core/closure.cmo");
-    Interpreter.Main.addExtension (baseDir ^ "/extensions/core/exception.cmo");
-    Interpreter.Main.addExtension (baseDir ^ "/extensions/core/phpString.cmo");
-    Interpreter.Main.addExtension (baseDir ^ "/extensions/core/files.cmo");
-    Interpreter.Main.addExtension (baseDir ^ "/extensions/core/info.cmo");
-    Interpreter.Main.addExtension (baseDir ^ "/extensions/core/phpArray.cmo");
+    Interpreter.Main.addExtension (baseDir ^ "/extensions/core.cmo");
     Interpreter.Main.addExtension (baseDir ^ "/extensions/spl.cmo");
     let filename =
         if Array.length Sys.argv = 2 then
